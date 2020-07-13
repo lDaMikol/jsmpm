088d0b5bdb5c2c505bb27a89a4c97fe889537250
Dzmitry Mishchanko <Dzmitry_Mishchanko@epam.com>
2020-07-13 20:29:34 +0300

    Objects commit

--- js-mentoring-program-master/tasks/objects/objects.js
+++ js-mentoring-program-master/tasks/objects/objects.js
@@ -2,19 +2,30 @@
  * create object person with firstName and lastName properties
  * implement method getFullName that returns full name
  */
-const person = //put you object here
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
 
 /**
  * create object student that has property grade and method getGrade
  * set person as prototype of student
  */
-const student = //put you object here
-
+const student = {
+    grade: 2,
+    getGrade() {
+        return this.grade;
+    }
+}
+Object.setPrototypeOf(student, person);
 /**
  * create new instance of student using Object.create
  */
-const student2 = //put you object here
-
+const student2 = Object.create(student); // put you object here
+ 
 module.exports = {
     person,
     student,
