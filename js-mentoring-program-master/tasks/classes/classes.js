3fe5e7d9115df2f19db285cb82abdb807fe34de1
Dzmitry Mishchanko <Dzmitry_Mishchanko@epam.com>
2020-07-21 17:44:36 +0300

    Commit classes

--- js-mentoring-program-master/tasks/classes/classes.js
+++ js-mentoring-program-master/tasks/classes/classes.js
@@ -5,7 +5,16 @@
  * 3) method getFullName
  */
 //put your code here
-
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+ 
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
 /**
  * implement class Student that extends Person with:
  * 1) property grade
@@ -13,10 +22,21 @@
  * 3) method getGrade that returns property grade
  */
 //put your code here
-
-const person = //create instance of class person
-const student = //create instance of class student
-
+ 
+class Student extends Person {
+    constructor(firstName, lastName, grade) {
+        super(firstName, lastName);
+        this.grade = grade;
+    }
+ 
+    getGrade() {
+        return this.grade;
+    }
+}
+ 
+const person = new Person('John', 'Doe');//create instance of class person\
+const student = new Student('John', 'Doe', 2);//create instance of class student
+ 
 module.exports = {
     person,
     student
