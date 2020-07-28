f734e3a71adfd04993a8fac6daeeed67aa747df8
Dzmitry Mishchanko <Dzmitry_Mishchanko@epam.com>
2020-07-29 02:24:19 +0300

    Async Commit

--- js-mentoring-program-master/tasks/async/async.js
+++ js-mentoring-program-master/tasks/async/async.js
@@ -4,43 +4,66 @@ const {
     callbackFunctionError,
     promiseFunctionError
 } = require("./async_api");
-
+ 
 /**
  * console.log data got from callbackFunction
  */
 function printDataCallback() {
+    const cb = (str, data) => console.log(data);
+    callbackFunction(cb);
 }
-
+ 
 /**
  * console.log data got from promiseFunction
  */
 function printDataPromise() {
+    return promiseFunction()
+        .then(value => console.log(value));
 }
-
+ 
 /**
  * console.log data got from promiseFunction using async/await
  */
 async function printDataAsyncAwait() {
+    const data = await promiseFunction();
+    console.log(data);
 }
-
+ 
 /**
  * throw error data from callbackFunctionError
  */
 function handleErrorCallback() {
+    const cb = (error) => {
+        throw error;
+    };
+    callbackFunctionError(cb);
 }
-
+ 
 /**
  * throw error come from promiseFunctionError
  */
+ 
 function handlePromiseError() {
+    try {
+        promiseFunctionError()()
+          .then(r => console.log(`.then(${r})`))
+          .catch(e => console.error(`.catch(${e})`));
+      } catch (e) {
+        throw e;
+      }
 }
-
+ 
 /**
  * throw error come from promiseFunctionError using async/await
  */
 async function handleAsyncAwaitError() {
+    try {
+        await promiseFunctionError();
+      } catch (error) {
+        throw error;
+      }
 }
-
+ 
 module.exports = {
     printDataCallback,
     printDataPromise,
